362cd14dfe5db7891f7ea93568e1e30e572a817e
MasanaIto <z3heng0326@gmail.com>
2020-07-15 23:33:41 +0900

    feature：タスクの追加と一覧を作成

--- index.js
+++ index.js
@@ -1,3 +1,42 @@
 'use strict'
 // { name: タスクの文字列, state: 完了しているかどうかの真偽値 }
 const tasks = new Array();
+
+/**
+ * TODOを追加する
+ * @param {string} task
+ */
+function todo(task) {
+    tasks.push({ name: task, state: false});
+}
+
+/**
+ * タスクと完了したかどうかが含まれるオブジェクトを受け取り、完了したかを返す
+ * @param {object} taskAndIsDonePair
+ * @return {boolean} 完了したかどうか
+ */
+function isDone(taskAndIsDonePair) {
+    return taskAndIsDonePair.state;
+}
+
+/**
+ * タスクと完了したかどうかが含まれるオブジェクトを受け取り、完了していないかを返す
+ * @param {object} taskAndIsDonePair
+ * @return {boolean} 完了していないかどうか
+ */
+function isNotDone(taskAndIsDonePair) {
+    return !isDone(taskAndIsDonePair);
+}
+
+/**
+ * TODOの一覧の配列を取得する
+ * @return {array}
+ */
+function list() {
+    return tasks.filter(isNotDone).map(t => t.name);
+}
+
+module.exports = {
+    todo,
+    list
+};
